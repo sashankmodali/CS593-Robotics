8dcede105b7b689a5ddb0cc85b18af93874866a4
sashankmodali <sashankmodali@gmail.com>
2022-04-28 23:06:16 -0400

    Added Assignment 4

--- Assignment 4/nmodali/Discrete/config.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-env_name = 'CartPole-v1'
-gamma = 0.99
-lr = 0.001
-log_interval = 10
-device = torch.device("cpu" if torch.cuda.is_available() else "cpu")
-env_seed = 500
-torch_seed = 500
--- Assignment 4/nmodali/Discrete/problem1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-./problem1-part1.sh
-./problem1-part2.sh
